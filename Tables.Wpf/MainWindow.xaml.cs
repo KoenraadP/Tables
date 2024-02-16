a513e028142520508ab152f17731c4f8dd19b855
Koenraad Pecceu <koenraad.pecceu@student.howest.be>
2024-02-16 23:03:58 +0100

    Add multiplication exercise on selection of number

--- Tables.Wpf/MainWindow.xaml.cs
+++ Tables.Wpf/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace Tables.Wpf
     public partial class MainWindow : Window
     {
         Random random = new Random();
+        int answer;
 
         public MainWindow()
         {
@@ -29,6 +30,29 @@ namespace Tables.Wpf
             cmbSelection.Items.Add(4);
             cmbSelection.Items.Add(5);
             cmbSelection.Items.Add(10);
+
+            txtAnswer.Focus();
+        }
+
+        private void txtAnswer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                
+            }
+        }
+
+        private void cmbSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int tableNumber = (int)cmbSelection.SelectedItem;
+            lblQuestion.Content = GenerateQuestion(tableNumber);
+        }
+
+        private string GenerateQuestion(int tableNumber)
+        {
+            int randomNr = random.Next(1, 11);
+            answer = tableNumber * randomNr;
+            return $"{randomNr} X {tableNumber}";
         }
     }
 }
