cfd8367bd35272e0edd6ef0c48d2d5dca5c12048
Koenraad Pecceu <koenraad.pecceu@student.howest.be>
2024-02-17 11:23:08 +0100

    Add minor UI changes and ability to keep progressing with enter

--- Tables.Wpf/MainWindow.xaml.cs
+++ Tables.Wpf/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace Tables.Wpf
     {
         Random random = new Random();
         int answer;
+        int tableNumber;
 
         public MainWindow()
         {
@@ -26,41 +27,68 @@ namespace Tables.Wpf
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            cmbSelection.Items.Add("Alles");
             cmbSelection.Items.Add(2);
             cmbSelection.Items.Add(4);
             cmbSelection.Items.Add(5);
             cmbSelection.Items.Add(10);
 
             txtAnswer.Focus();
+
+            cmbSelection.SelectedIndex = 0;
         }
 
         private void txtAnswer_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Enter)
-            {
+            if (e.Key == Key.Enter && !string.IsNullOrEmpty(txtAnswer.Text))
+            {                
                 int userAnswer = int.Parse(txtAnswer.Text);
                 if (userAnswer == answer)
                 {
                     lblQuestion.Background = Brushes.Green;
+                    btnNext.IsEnabled = true;
+                    txtAnswer.Clear();
+                    btnNext.Focus();
                 }
                 else
                 {
                     lblQuestion.Background = Brushes.Red;
-                }
+                    Clear();
+                }                
             }
         }
 
         private void cmbSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int tableNumber = (int)cmbSelection.SelectedItem;
-            lblQuestion.Content = GenerateQuestion(tableNumber);
+            if(cmbSelection.SelectedIndex != 0)
+            {
+                tableNumber = (int)cmbSelection.SelectedItem;
+                GenerateQuestion(tableNumber);
+                lblQuestion.Background = Brushes.Transparent;
+                Clear();
+            }            
         }
 
-        private string GenerateQuestion(int tableNumber)
+        private void GenerateQuestion(int tableNumber)
         {
             int randomNr = random.Next(1, 11);
             answer = tableNumber * randomNr;
-            return $"{randomNr} X {tableNumber}";
+            lblQuestion.Background = Brushes.Transparent;
+            Clear();
+            btnNext.IsEnabled = false;
+            lblQuestion.Content = $"{randomNr} X {tableNumber}";
+        }
+
+        private void Clear()
+        {            
+            txtAnswer.Clear();
+            txtAnswer.Focus();
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateQuestion(tableNumber);
+            lblQuestion.Background = Brushes.Transparent;
         }
     }
 }
